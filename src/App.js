9c9c4933b18ed27d1433b202cfd74889ca1bd6a9
Adrien Surowiec <adrien.surowiec@protonmail.com>
2023-04-12 20:34:06 +0200

    generate a random array of 16 key value pairs

--- src/App.js
+++ src/App.js
@@ -6,20 +6,30 @@ import Cards from "./Cards";
 function App() {
   const [currentScore, setCurrentScore] = useState(0);
   const [highestScore, setHighestScore] = useState(0);
+  const [sixteenCountries, setSixteenCountries] = useState([]);
+  const [allCountries, setAllCountries] = useState([]);
   const [countryCodes, setCountryCodes] = useState("fr");
   const [randomFlag, setRandomFlag] = useState(
     "https://flagcdn.com/w160/za.png"
   );
+  let allCountrycodes = [];
 
   useEffect(() => {
     const fetchCountryCodes = () => {
       fetch("https://flagcdn.com/en/codes.json")
         .then((response) => response.json())
         .then((data) => {
-          console.log(data);
-          let numberOfCountries = Object.keys(data).length;
-          let randomNumber = Math.floor(Math.random() * numberOfCountries);
-          setCountryCodes(Object.keys(data)[randomNumber]);
+          let allCountries = [...Object.entries(data)];
+          let shuffledCountries = allCountries.sort(
+            (a, b) => 0.5 - Math.random()
+          );
+          for (let i = 0; i < 16; i++) {
+            sixteenCountries.push(shuffledCountries[i]);
+          }
+          console.log(sixteenCountries);
+          // let numberOfCountries = Object.keys(data).length;
+          // let randomNumber = Math.floor(Math.random() * numberOfCountries);
+          // setCountryCodes(Object.keys(data)[randomNumber]);
         })
         .catch((error) => console.error(error));
     };
@@ -33,8 +43,8 @@ function App() {
 
   return (
     <div className="App">
-      <Scoreboard />
-      <Cards />
+      <Scoreboard currentScore={currentScore} highestScore={highestScore} />
+      <Cards sixteenCountries={sixteenCountries} />
     </div>
   );
 }
--- src/Cards.js
+++ src/Cards.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { useState, useEffect } from "react";
 
-export default function Cards() {
+export default function Cards(props) {
+  const [sixteenCountries, setSixteenCountries] = useState(
+    props.sixteenCountries
+  );
+
   return <div>Cards</div>;
 }
