07d937a25eac6a4f241c40da3cad70e081a44c93
Adrien Surowiec <adrien.surowiec@protonmail.com>
2023-04-13 13:05:22 +0200

    generate 16 countries and display them with flags

--- src/Cards.js
+++ src/Cards.js
@@ -7,90 +7,22 @@ export default function Cards(props) {
     props.sixteenCountries
   );
 
-  //   testarray = [
-  //     ["cd", "DR CONGO"],
-  //     ["fr", "FRANCE"],
-  //   ];
-
-  //   const list = testarray.map((country) => (
-  //     <div>
-  //       <img
-  //         className="flag"
-  //         src={`https://flagcdn.com/w160/${country[0]}.png`}
-  //         alt=""
-  //       />
-  //       <p>{country[1]}</p>
-  //     </div>
-  //   ));
-
   return (
     <div className="cardSpace">
-      <p>{props.sixteenCountries}</p>
-      <button onClick={props.updateCountries}></button>
-      <div>
-        <img className="flag" src="https://flagcdn.com/w160/za.png" alt="" />
-        <p>South Africa</p>
-      </div>
-      <div>
-        <img className="flag" src="https://flagcdn.com/w160/fr.png" alt="" />
-        <p>France</p>
-      </div>
-      <div>
-        <img src="" alt="" />
-        <p></p>
-      </div>
-      <div>
-        <img src="" alt="" />
-        <p></p>
-      </div>
-      <div>
-        <img src="" alt="" />
-        <p></p>
-      </div>
-      <div>
-        <img src="" alt="" />
-        <p></p>
-      </div>
-      <div>
-        <img src="" alt="" />
-        <p></p>
-      </div>
-      <div>
-        <img src="" alt="" />
-        <p></p>
-      </div>
-      <div>
-        <img src="" alt="" />
-        <p></p>
-      </div>
-      <div>
-        <img src="" alt="" />
-        <p></p>
-      </div>
-      <div>
-        <img src="" alt="" />
-        <p></p>
-      </div>
-      <div>
-        <img src="" alt="" />
-        <p></p>
-      </div>
-      <div>
-        <img src="" alt="" />
-        <p></p>
-      </div>
-      <div>
-        <img src="" alt="" />
-        <p></p>
-      </div>
-      <div>
-        <img src="" alt="" />
-        <p></p>
-      </div>
-      <div>
-        <img src="" alt="" />
-        <p></p>
-      </div>
+      {props.sixteenCountries.map((country) => (
+        <div>
+          <img
+            className="flag"
+            src={`https://flagcdn.com/w160/${country[0]}.png`}
+            alt=""
+          />
+          <p>{country[1]}</p>
+        </div>
+      ))}
+
+      <button onClick={props.updateCountries}>
+        Generate 16 random countries
+      </button>
     </div>
   );
 }
